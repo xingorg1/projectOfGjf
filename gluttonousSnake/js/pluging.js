a94502c33539841a4f666e5eaf13587fd1d10f50
xing.org1^ <xing.org1@outlook.com>
2019-06-08 17:35:45 +0800

    贪吃蛇
    优化

--- gluttonousSnake/js/pluging.js
+++ gluttonousSnake/js/pluging.js
@@ -2,9 +2,81 @@
  * @Author: @Guojufeng 
  * @Date: 2018-12-14 16:22:28 
  * @Last Modified by: @Guojufeng
- * @Last Modified time: 2019-02-01 18:06:37
+ * @Last Modified time: 2019-06-08 17:29:58
  */
 var xingorg1Utils = {
+  /* 记录最高分 */
+  wls: {
+    ls: window.localStorage,
+    getItem(key) {
+      try {
+        return JSON.parse(this.ls.getItem(key))
+      } catch (err) {
+        return null
+      }
+    },
+    setItem(key, val) {
+      this.ls.setItem(key, JSON.stringify(val))
+    },
+    clear() {
+      this.ls.clear()
+    },
+    keys() {
+      return this.ls.keys()
+    },
+    removeItem(key) {
+      this.ls.removeItem(key)
+    }
+  },
+  /* 获取两对坐标差的平方和 */
+  calLength2(x1, y1, x2, y2) {
+    // pow：开平方，2是次数
+    return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+  },
+  /* 移动方向监测 */
+  GetSlideDirection(startX, startY, endX, endY) {
+    /* from https://www.cnblogs.com/yangmengsheng/p/5973487.html */
+    //根据起点和终点返回方向 1：向上，2：向下，3：向左，4：向右,0：未滑动
+    var dy = startY - endY;
+    var dx = endX - startX;
+    var result = 0;
+    //如果滑动距离太短
+    if (Math.abs(dx) < 2 && Math.abs(dy) < 2) {
+      return result;
+    }
+    var angle = this.GetSlideAngle(dx, dy);
+    if (angle >= -45 && angle < 45) {
+      result = 4;
+    } else if (angle >= 45 && angle < 135) {
+      result = 1;
+    } else if (angle >= -135 && angle < -45) {
+      result = 2;
+    } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+      result = 3;
+    }
+    return result;
+  },
+  GetSlideAngle(dx, dy) {
+    return Math.atan2(dy, dx) * 180 / Math.PI;
+  },
+  /* 设备检测 */
+  isPhone: () => {
+    /* from https://www.cnblogs.com/laq627/p/5848680.html */
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+      return true;
+    } else {
+      return false;
+    }
+  },
   /* 获取随机颜色值 */
   getRandomColor: () => {
     var color = '#',
